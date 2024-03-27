156feca25fc9a77a05ae5e9efc03a4cb739f9ade
Guk Jin Han <rnrwls2482@naver.com>
2024-03-27 19:48:29 +0900

    Changes

---
+++ src/Plus.java
@@ -0,0 +1,11 @@
+public class Plus {
+    public static void main(String[] args) {
+
+
+        int a = 12;
+        int b = 29;
+        int result = a + b;
+
+        System.out.println(result);
+    }
+}
